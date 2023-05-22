afdd0105757de332c75689a685d77ea21da164d9
Maria <mashabiryukova20142405@gmail.com>
2023-05-22 16:02:55 +0300

    commit 2

---
+++ src/main/java/org/example/NewFile.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class NewFile {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+    public static String method2(String name2){
+        return "Hello"+name2;
+    }
+
+    public static String method3(String name3){
+        return "Hello"+name3;
+    }
+}
