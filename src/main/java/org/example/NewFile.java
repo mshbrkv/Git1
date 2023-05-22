8f4c63b911135451e1e30f3e0a109178854f55b0
Maria <mashabiryukova20142405@gmail.com>
2023-05-22 16:03:46 +0300

    commit 3

--- src/main/java/org/example/NewFile.java
+++ src/main/java/org/example/NewFile.java
@@ -5,11 +5,7 @@ public class NewFile {
         System.out.println("Hello world!");
     }
 
-    public static String method2(String name2){
+    public static String method2edit(String name2){
         return "Hello"+name2;
-    }
-
-    public static String method3(String name3){
-        return "Hello"+name3;
     }
 }
