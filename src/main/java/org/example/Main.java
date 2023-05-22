8e1b3c23c37d35bda07562471114572ca5d07776
Maria <mashabiryukova20142405@gmail.com>
2023-05-22 15:57:32 +0300

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,8 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static String method1(){
+        return "Hello";
+    }
 }
